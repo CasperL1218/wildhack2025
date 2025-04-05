5d12f866d8747f7c519952ea19cec611798b3ff2
belenedgar <belenedgar00@gmail.com>
2025-04-05 15:27:00 -0500

    updated main.py with flask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import streamlit as st
+from flask import Flask, request, jsonify
 import os
 
 from PIL import Image
@@ -9,21 +9,23 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel('gemini-pro-vision')
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+app = Flask(__name__)
 
 def get_gemini_response(input, image, prompt):
     try:
         response = model.generate_content([input, image[0], prompt])
-        return response.text
+        return response
     except Exception as e:
         return f"Error: {str(e)}"
 
 def input_image_details(uploaded_file):
     if uploaded_file is not None:
-        bytes_data = uploaded_file.file.read()
+        bytes_data = uploaded_file.read()
         image_parts = [
             {
-                "mime_type": uploaded_file.type,
+                "mime_type": uploaded_file.content_type,
                 "data": bytes_data
             }
         ]
@@ -50,11 +52,34 @@ def scan_food(uploaded_file):
         # Get the response from the generative model
         response = get_gemini_response(input_prompt, image_data, input_prompt)
         
-        if "error" in response:
-            return response  # Return any error message from the response
-        return response
+        if isinstance(response, str):
+            return response  # Return if it's already a string
+        else:
+            return str(response)  # Convert the response to a string
 
     except FileNotFoundError:
         return {"error": "No image file uploaded"}
     except Exception as e:
         return {"error": str(e)}
+    
+
+@app.route('/scan-food', methods=['POST'])
+def scan_food_endpoint():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"})
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({"error": "No selected file"})
+    
+    try:
+        # Call the scan_food function with the uploaded file
+        result = scan_food(file)
+        # return jsonify(result)
+        return jsonify({"response": result})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
